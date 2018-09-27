31da84672921ca3c55609bed2eed46c5f478aade
Hayyaan <43389394+hayyaanHCSS@users.noreply.github.com>
2018-09-27 09:53:25 -0500

    Update PullLatestInstall.bat

--- Script/PullLatestInstall.bat
+++ Script/PullLatestInstall.bat
@@ -1,4 +1,4 @@
 @echo on
 for /F "delims=" %%a in ('dir  /b /od "\\HBbuild\Installs\master\*.msi"') do set Youngest=%%a
-xcopy /y "\\HBbuild\Installs\master\*.msi\%Youngest%" "C:\Users\hayyaan.merchant\Desktop\BATCH" 
-start C:\Users\hayyaan.merchant\Desktop\BATCH\%Youngest%"
+xcopy /y "\\HBbuild\Installs\master\*.msi\%Youngest%" "C:\Users\hayyaan.merchant\Downloads\BATCH" 
+start C:\Users\hayyaan.merchant\Downloads\BATCH\%Youngest%"
